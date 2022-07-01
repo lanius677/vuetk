726b6c8b5e9a6d9b6d98b849214fb1d898fa023a
lanius <lanius@foxmail.com>
2022-07-01 23:47:18 +0800

    前端项目front初始化

---
+++ front/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>tk课程</p>
+    <el-button type="primary">Primary</el-button>
+    <el-icon><Check /></el-icon>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped lang="less">
+p {
+  color: skyblue;
+}
+</style>
